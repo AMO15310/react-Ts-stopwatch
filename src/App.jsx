0e7e342a6e67a38b71345d8819bd5f5c993756e2
AMO15310 <wachiraamos402@gmail.com>
2022-11-17 20:03:04 +0300

    Upload file

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,18 @@ import "./App.css";
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Timer from "./components/timer";
-import Buttons from "./components/buttons";
+import Buttons from "./components/butttons2";
 
 class myApp extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      mins: 0,
-      hours: 0,
-      seconds: 0,
-    };
   }
   render() {
     return (
       <>
         <div className="comp">
           <p className="title">Stop Watch</p>
-          <Buttons data={this.state} />
+          <Buttons />
         </div>
       </>
     );
